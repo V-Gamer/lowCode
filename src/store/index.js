4ba47e8106b0826ea70f7aa92c3f2d4cba075aee
PC-202207290100\Administrator <355665195@qq.com>
2022-08-06 00:31:30 +0800

    创建和引入store文件

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from "vue"
+import Vuex from "vuex" //引入vuex
+Vue.use(Vuex) //使用vuex
+
+export default new Vuex.Store({
+  state: {
+    num: "我是store中的数据",//实现参数传递的
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+
+  }
+})
