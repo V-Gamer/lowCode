a6cf497c19574592c9525db395be14c6d394e112
closererererer <2643573963@qq.com>
2022-08-12 14:44:50 +0800

    wml拖拽实现，仅实现拖拽功能，各种组件未完成

---
+++ src/components/component/v-button.vue
@@ -0,0 +1,32 @@
+<!-- v-button组件 -->
+<template>
+  <input type="button" class="button" :value="propValue" />
+</template>
+
+<script>
+export default {
+  name: "editPage",
+  props: {
+    editMod: String, // 组件内容是否可修改
+    propValue: String, // 组件的内容（例如文字等等）
+  },
+  data() {
+    return {
+      url:"ann",
+
+    };
+  },
+  methods: {
+    // textarea输入事件
+    fn_handleInput() {},
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+ .button {
+  width: 40px;
+  height: 40px;
+ }
+</style>
